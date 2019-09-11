8ee3a3dac91aff426c2112a0b503c63075f074db
mittulmandhan <conanmittul@gmail.com>
2019-09-11 06:49:49 +0530

    HttpHeaders added

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 // UserService is created for calling the api method which will give us all users details list in response
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
@@ -11,8 +11,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
+  // when calling a rest API we need to determine the type of data we are sending
+  // for that purpose we define HttpHeaders
+  headers: HttpHeaders;
+
   // HttpClient service is used to make ajax request in angular
   constructor(private httpClient: HttpClient) {
+    this.headers = new HttpHeaders({'content-type': 'application/ json'});
   }
 
   // GetUsers method should be called when we are going to subscribe jsn in the component level
@@ -24,14 +29,15 @@ export class UserService {
   }
 
   GetUser(id: number): Observable<User> {
-    return this.httpClient.get<User>(environment.apiAddress + '/users' + id);
+    return this.httpClient.get<User>(environment.apiAddress + '/users/' + id);
   }
 
-  AddUser(user: User): Observable<any> {
-    return this.httpClient.post<any>(environment.apiAddress + '/users', JSON.stringify(user));
+  AddUser(user: User): Observable<HttpResponse<any>> {
+    // tslint:disable-next-line: max-line-length
+    return this.httpClient.post<HttpResponse<any>>(environment.apiAddress + '/users', JSON.stringify(user), {headers: this.headers, observe: 'response'});
   }
 
   DeleteUser(id: number): Observable<any> {
-    return this.httpClient.delete<any>(environment.apiAddress + '/users' + id);
+    return this.httpClient.delete<any>(environment.apiAddress + '/users/' + id);
   }
 }
