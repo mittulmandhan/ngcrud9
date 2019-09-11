fde574d732aa2beeee4cc1aab420a75561be0d9d
mittulmandhan <conanmittul@gmail.com>
2019-09-11 16:17:16 +0530

    Delete method added to remove a user

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -8,16 +9,26 @@ import { UserService } from '../services/user.service';
 })
 export class UsersComponent implements OnInit {
   data: any[];
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {
+
+   }
 
   ngOnInit() {
     this.userService.GetUsers().subscribe(res => {
       this.data = res;
     });
   }
-
   DeleteUser(id: number) {
-
+    if (confirm('Are you sure to delete?')) {
+      this.userService.DeleteUser(id).subscribe(res => {
+        if (res.status === 200) {
+          for (let i = 0; i < this.data.length; i++) {
+            if (id === this.data[i].id) {
+              this.data.splice(i, 1);
+            }
+          }
+        }
+      });
+    }
   }
-
 }
